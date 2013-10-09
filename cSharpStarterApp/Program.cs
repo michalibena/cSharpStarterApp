ff6209a943ae4fa6f769a7bc770f6bff9905e093
robmarano <robmarano@gmail.com>
2013-10-09 19:24:01 -0400

    First deployment after creating the C# solution via Visual Studio 2012

---
+++ cSharpStarterApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cSharpStarterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
