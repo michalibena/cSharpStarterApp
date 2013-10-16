8aa999d937b79a6de0ad5d8f770cbc2fa7ced5b6
robmarano <robmarano@gmail.com>
2013-10-16 19:38:43 -0400

    Added Worker.cs class to demonstrate the following: classes, objects, instantiation, ctors, fields, properties, get/set methods, access modifiers (private/public), class methods, string class, override the object base class to print out the class for debugging, override demonstrates C#'s ability to do polymorphism, an OO characteristic

--- cSharpStarterApp/Program.cs
+++ cSharpStarterApp/Program.cs
@@ -2,20 +2,25 @@
 
 namespace cSharpStarterApp
 {
+    // our wrapper class to simply run our code as modeled by the classes.
+    //
+    // What did we learn today:
+    // classes, objects, instantiation, ctors, fields, properties, get/set methods,
+    // access modifiers (private/public), class methods, string class,
+    // override the object base class to print out the class for debugging,
+    // override demonstrates C#'s ability to do polymorphism, an OO characteristic
     class Program
     {
         static void Main(string[] args)
         {
             int loopLength = 5;
-            int i;
+            string message = "Hello, World!";
 
-            for (i = 0; i < loopLength; i++)
-            {
-                if ((i % 2) == 0)
-                {
-                    Console.WriteLine(i + ": Hello, World!");
-                }
-            }
+            // worker is an object reference to the class Worker;
+            // the Worker ctor instantiates the object worker.
+            Worker worker = new Worker(loopLength, message);
+            Console.WriteLine(worker);
+            worker.doWork();
         }
     }
 }
---
+++ cSharpStarterApp/Worker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace cSharpStarterApp
+{
+    class Worker
+    {
+        // class fields/members
+        int _count;
+        string _message;
+
+        // class properties
+        int Count
+        //{ get; set;}
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+            }
+        }
+
+        string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+            }
+        }
+
+        // ctors
+        public Worker(int count, string message)
+        {
+            Count = count;
+            Message = message;
+        }
+
+        // setters - if not using properties
+        //void setMessage(string message) { }
+        //void setCount(int count) { }
+
+        // getters - if not using properties
+        //int getCount() { return _count; }
+        //string getMessage() { return _message; }
+
+        public void doWork()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(i + ": "+ Message);
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = base.ToString() + ": Count = " + Count + ", Message = " + Message;
+            return s;
+        }
+    }
+}
